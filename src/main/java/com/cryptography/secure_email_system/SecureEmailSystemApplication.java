83438d855170b3bed9495d56a10480464f40acbd
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-12-20 23:40:37 +0300

    Initial commit

---
+++ src/main/java/com/cryptography/secure_email_system/SecureEmailSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cryptography.secure_email_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureEmailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureEmailSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptography/secure_email_system/SecureEmailSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptography.secure_email_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureEmailSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
